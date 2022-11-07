beac9cb1b3e6fdd98ec4892e83fffd0a8c31cf78
ShenHdian <15574380273@163.com>
2022-11-08 00:04:38 +0800

    hot-fix first commit

--- idea-git-test/src/com/nwpu/gitdemo/gitdemo.java
+++ idea-git-test/src/com/nwpu/gitdemo/gitdemo.java
@@ -12,5 +12,6 @@ public class gitdemo {
         System.out.println("git-idea 搭建test1");
         System.out.println("git-idea 搭建test2");
         System.out.println("git-idea 搭建test3");
+        System.out.println("git-idea hot-fix have changed");
     }
 }
