ebfef323dd64ee81bc39c47d5a87dd3d7eb9f888
ShenHdian <15574380273@163.com>
2022-11-08 00:00:29 +0800

    master first commit

---
+++ idea-git-test/src/com/nwpu/gitdemo/gitdemo.java
@@ -0,0 +1,10 @@
+package com.nwpu.gitdemo;
+
+public class gitdemo {
+    public static void main(String[] args) {
+        System.out.println("git-idea 搭建test");
+        System.out.println("git-idea 搭建test");
+        System.out.println("git-idea 搭建test");
+        System.out.println("git-idea 搭建test");
+    }
+}
