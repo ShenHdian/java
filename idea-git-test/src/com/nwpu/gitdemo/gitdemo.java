642309f7885d6962b621af89bfcad5caeefe9dad
ShenHdian <15574380273@163.com>
2022-11-08 00:02:06 +0800

    master third commit

--- idea-git-test/src/com/nwpu/gitdemo/gitdemo.java
+++ idea-git-test/src/com/nwpu/gitdemo/gitdemo.java
@@ -8,5 +8,9 @@ public class gitdemo {
         System.out.println("git-idea 搭建test");
         System.out.println("git-idea 搭建test");
         System.out.println("git-idea 搭建test");
+        System.out.println("git-idea 搭建test");
+        System.out.println("git-idea 搭建test1");
+        System.out.println("git-idea 搭建test2");
+        System.out.println("git-idea 搭建test3");
     }
 }
